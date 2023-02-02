c597c07b5e3fedffe76bbc70b0d8ce64ad55bc61
admiralAwkbar <gravley@stripe.com>
2023-02-02 14:47:16 -0600

    adding code

--- index.js
+++ index.js
@@ -7,44 +7,63 @@
 // Requirments
 const csv = require('csvtojson')
 const fs = require('fs')
-
+const converter = csv({
+  trim: true,
+  ignoreEmpty: true
+})
+const main = async () => {
 /**
  * Variables
 */
-// Get the path to the CSV file from parameters
-const csvFilePath = process.argv[2]
-// Get the path to the JSON file from parameters
-const jsonFilePath = process.argv[3]
+  // Get the path to the CSV file from parameters
+  const csvFilePath = process.argv[2]
+  // Get the path to the JSON file from parameters
+  const jsonFilePath = process.argv[3]
 
-/**
+  /**
  * Validate the parameters
 */
-if (!csvFilePath) {
-  console.log('Please provide a valid path to the CSV file')
-}
-if (jsonFilePath) {
-  console.log('A file named ' + jsonFilePath + ' already exists')
-  console.log('Please provide a valid path to the JSON file')
-}
-/**
+  if (!csvFilePath) {
+    console.log('ERROR: Please provide a valid path to the CSV file...')
+    // exit the program
+    process.exit(1)
+  }
+  if (!jsonFilePath) {
+    console.log('WARN: No JSON file path given, defaulting to "output.json"...')
+    // Set the default JSON file path
+    jsonFilePath = 'output.json'
+  }
+  /**
  * Convert the CSV file to JSON file
  */
-csv()
-  .fromFile(csvFilePath)
-  .then((jsonObj) => {
-    console.log(jsonObj)
+  csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+      console.log(jsonObj)
     /**
      * [
      * \s{a:"1", b:"2", c:"3"},
      * \s{a:"4", b:"5". c:"6"}
      * ]
     */
+    })
+
+  // Async / await usage
+  const jsonArray = await csv().fromFile(csvFilePath)
+  // Write the jsonArry to the JSON file
+  const file = fs.createWriteStream(jsonFilePath)
+  file.write('[')
+  // error handling
+  file.on('error', function (err) { console.log(err) })
+  jsonArray.forEach(function (v) {
+    // dont write the line if the value is empty
+    if (v) { file.write(`${JSON.stringify(v)},\n`) }
   })
 
-// Async / await usage
-const jsonArray = await csv().fromFile(csvFilePath)
-// Write the jsonArry to the JSON file
-const file = fs.createWriteStream(csvFilePath)
-file.on('error', function (err) { console.log(err) })
-jsonArray.forEach(function (v) { file.write(v.join(', ') + '\n') })
-file.end()
+  // remove the last comma
+  file.write('\b\b')
+  file.write(']')
+  file.end()
+}
+
+main()
